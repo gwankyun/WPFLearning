50d04d32cdf6ece2b484a7359209b609933f5262
權少 <ljqican@163.com>
2019-09-04 22:41:38 +0800

    初始化頁目，添加新增條目功能

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLearning
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            users.Add(new User() { Name = "Tom" });
+            users.Add(new User() { Name = "Jim" });
+            lvUsers.ItemsSource = users;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var content = tbxContent.Text;
+            if (content != "")
+            {
+                users.Add(new User() { Name = content });
+                tbxContent.Text = "";
+            }
+        }
+    }
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
